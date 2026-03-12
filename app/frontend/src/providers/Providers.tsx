40cbe84175df06cae2012ee439b978f66a956fb4
Jaka Krajnc <jaka.krajnc.jk@gmail.com>
2026-03-12 20:32:35 +0100

    feat: add tanstack provider

--- app/frontend/src/providers/Providers.tsx
+++ app/frontend/src/providers/Providers.tsx
@@ -1,12 +1,19 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Provider } from 'react-redux';
 import { store } from '@/stores/store';
+
+const queryClient = new QueryClient();
 
 interface IProviders {
   children: React.ReactNode;
 }
 
 export default function Providers({ children }: IProviders) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Provider store={store}>{children}</Provider>
+    </QueryClientProvider>
+  );
 }
