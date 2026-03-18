ebca7b14a189c4b42bc8cc6d1216e7521407c668
Iva <74246013+Iborovi03@users.noreply.github.com>
2026-03-18 23:18:47 +0100

    sprememba poimenovanj

--- app/frontend/src/stores/store.ts
+++ app/frontend/src/stores/store.ts
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
 import userReducer from './slices/userSlice';
-import dolgReducer from './slices/dolgSlice';
+import debtReducer from './slices/debtSlice';
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    dolg: dolgReducer,
+    debt: debtReducer,
   },
 });
 
